037cb208cfba11d0568786ef706d0a5abfce6fdd
truongduchuy910 <truongduchuy910@gmail.com>
2020-01-12 15:48:07 +0700

    done keystone

--- lists/products.js
+++ lists/products.js
@@ -2,7 +2,7 @@ const { Text, Relationship, Slug, File } = require("@keystonejs/fields");
 const { Markdown } = require("@keystonejs/fields-markdown");
 const { LocalFileAdapter } = require("@keystonejs/file-adapters");
 const fileAdapter = new LocalFileAdapter({
-  src: "./app/public/store",
+  src: "./public/store",
   path: "/store"
 });
 
